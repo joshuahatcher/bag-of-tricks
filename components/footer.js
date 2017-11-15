7e93196a4470a041ca07020e012ec4d8053f7086
Joshua Hatcher <joshua.abraham.hatcher@gmail.com>
2017-11-15 14:51:16 -0500

    Update footer

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,5 @@
 const Footer = (context) => (
-  `<footer>Copyright ${context.date}, ${context.name}</footer>`
+  `<footer>Copyright ${context.date}, ${context.name}. All rights reserved.</footer>`
 );
 
 export default Footer;
